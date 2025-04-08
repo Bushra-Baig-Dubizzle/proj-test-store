70fcb945920ca11d8da6209268a7c9ff00804865
Bushra-Baig-Dubizzle <bushra.baig@dubizzlelabs.com>
2025-04-08 14:34:04 +0500

    Adding subscribers table

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,12 @@
 class Product < ApplicationRecord
+    # Associations
+    has_many :subscribers, dependent: :destroy
+
+    # Virtual Attributes
     has_one_attached :featured_image
     has_rich_text :description
+
+    # Attributes
     validates :name, presence: true
     validates :inventory_count, numericality: { greater_than_or_equal_to: 0 }
   end
