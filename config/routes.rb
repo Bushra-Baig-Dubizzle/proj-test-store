0a4eaf95069b13fb91d2c3033915e4f09c052e46
Bushra-Baig-Dubizzle <bushra.baig@dubizzlelabs.com>
2025-04-08 12:03:36 +0500

    Adding edit and update actions

--- config/routes.rb
+++ config/routes.rb
@@ -25,9 +25,9 @@ Rails.application.routes.draw do
   post "/products", to: "products#create"
   get "/products/:id", to: "products#show", as: 'product'
 
-# get "/products/:id/edit", to: "products#edit"
-# patch "/products/:id", to: "products#update"
-# put "/products/:id", to: "products#update"
+  get "/products/:id/edit", to: "products#edit"
+  patch "/products/:id", to: "products#update"
+  put "/products/:id", to: "products#update"
 
 delete "/products/:id", to: "products#destroy"
 
