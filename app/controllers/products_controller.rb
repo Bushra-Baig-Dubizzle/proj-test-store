be893fe4f0e2a8dd70631851a62eab56d9377ab3
Bushra-Baig-Dubizzle <bushra.baig@dubizzlelabs.com>
2025-04-08 13:10:46 +0500

    Adding rich text

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -47,7 +47,7 @@ class ProductsController < ApplicationController
     def product_params
       # Strong Parameters: allow only :name to be submitted through forms for security
       # params.expect(product: [ :name ])
-      params.require(:product).permit(:name)
+      params.require(:product).permit(:name, :description)
     end
 
     def set_product
