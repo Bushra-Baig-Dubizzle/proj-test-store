778b5f1e2f00abb1d17e9760e4ab6e2011262471
Bushra-Baig-Dubizzle <bushra.baig@dubizzlelabs.com>
2025-04-08 12:21:02 +0500

    Resolving error for edit aliases issue.

--- config/routes.rb
+++ config/routes.rb
@@ -25,11 +25,11 @@ Rails.application.routes.draw do
   post "/products", to: "products#create"
   get "/products/:id", to: "products#show", as: 'product'
 
-  get "/products/:id/edit", to: "products#edit"
+  get "/products/:id/edit", to: "products#edit", as: 'edit_product'
   patch "/products/:id", to: "products#update"
   put "/products/:id", to: "products#update"
 
-delete "/products/:id", to: "products#destroy"
+  delete "/products/:id", to: "products#destroy"
 
 # resources :products for all actions
 
