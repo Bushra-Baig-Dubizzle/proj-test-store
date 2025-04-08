014da8680f870f4376fa0b21bfcb34b56cfcc410
Bushra-Baig-Dubizzle <bushra.baig@dubizzlelabs.com>
2025-04-08 12:06:32 +0500

    Replacing data with before_actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,13 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: %i[ show edit update ]
+
   def index
     @products = Product.where.not(name: [nil, ""])
   end
 
   def show
-    @product = Product.find(params[:id])
+    # @product = Product.find(params[:id])
+    # Now using before actions
   end
 
   def new
@@ -21,11 +24,11 @@ class ProductsController < ApplicationController
   end
 
   def edit
-    @product = Product.find(params[:id])
+    # @product = Product.find(params[:id])
+    # Now using before_actions
   end
 
   def update
-    @product = Product.find(params[:id])
     if @product.update(product_params)
       redirect_to @product
     else
@@ -38,6 +41,9 @@ class ProductsController < ApplicationController
       # Strong Parameters: allow only :name to be submitted through forms for security
       # params.expect(product: [ :name ])
       params.require(:product).permit(:name)
+    end
 
+    def set_product
+      @product = Product.find(params[:id])
     end
 end
