2ed577602faf1bf07da4dc875fadefbe3032936f
Bushra-Baig-Dubizzle <bushra.baig@dubizzlelabs.com>
2025-04-08 11:26:50 +0500

    Changing the aliases from 'create' to 'show'

--- config/routes.rb
+++ config/routes.rb
@@ -22,8 +22,8 @@ Rails.application.routes.draw do
   get 'products/new', to: 'products#new', as: 'new_product'
 
   
-  post "/products", to: "products#create", as: 'product'
-  get "/products/:id", to: "products#show"
+  post "/products", to: "products#create"
+  get "/products/:id", to: "products#show", as: 'product'
 
 # get "/products/:id/edit", to: "products#edit"
 # patch "/products/:id", to: "products#update"
