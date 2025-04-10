ef141edc786a47597e97a8bfdbdefb6898e3afa0
Bushra-Baig-Dubizzle <bushra.baig@dubizzlelabs.com>
2025-04-10 11:47:05 +0500

    Adding queue job to the project

---
+++ app/jobs/guests_cleanup_job.rb
@@ -0,0 +1,8 @@
+class GuestsCleanupJob < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+    puts ">>> GuestsCleanupJob is running with args: #{args.inspect}"
+    $stdout.flush
+  end
+end
